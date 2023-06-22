86692658eb9d89558e05f40845b01ff2b1edf56e
ManbirJudge <manbirjudge2009@gmail.com>
2023-06-22 21:38:57 +0530

    Fixing (mainly)

--- public/script.js
+++ public/script.js
@@ -1,94 +1,38 @@
-// const { PeerServer } = require("peer");
+// ---------------------------
+const socket = io('/')
+const peer = new Peer()
 
-const socket = io('/');
+const users = {}
 
-const videoGrid = document.getElementById('video-grid');
+// ---------------------------
+const joinConfirmDialog = document.getElementById('join-confirm-dialog')
+const nameInputForJoin = document.querySelector('#name-input-for-join')
+const joinBtn = document.getElementById('join-btn')
+const videoGrid = document.getElementById('video-grid')
+const currentUserVideo = document.createElement('video')
 
-const myVideo = document.createElement('video');
-myVideo.muted = true;
+let currentUserVideoStream
 
-let myVideoStream;
-
-var peer = new Peer(undefined, {
-   path: '/peerjs' ,
-   host: '/',
-   port: '443'
-});
+currentUserVideo.muted = true
 
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
 }).then(stream => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream)
-    
-    peer.on('call', call => {
-        call.answer(stream);
-
-        const video = document.createElement('video');
-
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream);
-        });
-    });
-
-    socket.on('user-connected', (userId) => {
-        connectToNewUser(userId, stream);
-    });
-
-    // Messages Logic
-    let text = $('input');
-
-    $('html').keydown(e => {
-        if (e.which == 13 && text.val().lenght !== 0) {
-            socket.emit('message', text.val());
-            text.val('');
-        }
-    });
-
-    socket.on('create-message', message => {
-        console.log('Some One Messaged:', message);
-
-        $('.messages').append(`<li class='message'><b>User</b><br/>${message}</li>`);
-        scrollToBottom();
-    });
+    currentUserVideoStream = stream
 });
 
-peer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id);
-});
+// connection function
+const addVideoStream = (video, stream, userName) => {
+    video.srcObject = stream
+    video.play()
 
-// socket.on('user-connected', (userId) => {
-//     connectToNewUser(userId, stream);
-// });
-
-const connectToNewUser = (userId, stream) => {
-    const call = peer.call(userId, stream);
-
-    const video = document.createElement('video');
-
-    call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream);
-    });
+    videoGrid.append(video)
 };
 
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream;
-
-    // video.addEventListener('loadmetadata', () => {
-        video.play();
-    // });
-
-    videoGrid.append(video);
-};
-
-const scrollToBottom = () => {
-    var d = $('.main__chat_window');
-    d.scrollTop(d.prop('scrollHeight'));
-};
-
-const muteUnmute = () => {
-    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+// toggle functions
+const toggleAudio = () => {
+    const enabled = currentUserVideoStream.getAudioTracks()[0].enabled;
 
     if (enabled) {
         setMuteButton();
@@ -96,11 +40,11 @@ const muteUnmute = () => {
         setUnmuteButton();
     }
 
-    myVideoStream.getAudioTracks()[0].enabled = !enabled;
+    currentUserVideoStream.getAudioTracks()[0].enabled = !enabled;
 }
 
-const startStopVideo = () => {
-    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+const toggleVideo = () => {
+    const enabled = currentUserVideoStream.getVideoTracks()[0].enabled;
 
     if (enabled) {
         setStartVideoButton();
@@ -108,12 +52,18 @@ const startStopVideo = () => {
         setStopVideoButton();
     }
 
-    myVideoStream.getVideoTracks()[0].enabled = !enabled;
+    currentUserVideoStream.getVideoTracks()[0].enabled = !enabled;
 }
 
+// ui update function
+const scrollToBottom = () => {
+    var d = $('.main__chat_window');
+    d.scrollTop(d.prop('scrollHeight'));
+};
+
 const setMuteButton = () => {
-    const html = 
-    `
+    const html =
+        `
         <i class="unmute fas fa-microphone-slash"></i>
         <span>Unmute</span>
     `;
@@ -122,8 +72,8 @@ const setMuteButton = () => {
 }
 
 const setUnmuteButton = () => {
-    const html = 
-    `
+    const html =
+        `
         <i class="fas fa-microphone"></i>
         <span>Mute</span>
     `;
@@ -132,8 +82,8 @@ const setUnmuteButton = () => {
 }
 
 const setStartVideoButton = () => {
-    const html = 
-    `
+    const html =
+        `
         <i class="fas fa-video-slash"></i>
         <span>Start Video</span>
     `;
@@ -142,11 +92,68 @@ const setStartVideoButton = () => {
 }
 
 const setStopVideoButton = () => {
-    const html = 
-    `
+    const html =
+        `
         <i class="fas fa-video"></i>
         <span>Stop Video</span>
     `;
 
     document.querySelector(".main__video_button").innerHTML = html;
 }
+
+// ---------------------------
+let chatMessageInput = $('#chat-message-input');
+
+chatMessageInput.keydown(e => {
+    const message = chatMessageInput.val();
+    if (e.which == 13 && message.lenght !== 0) {
+        console.log(`Sent message: ${message}`)
+        socket.emit('message', message);
+        chatMessageInput.val('');
+    }
+});
+
+// ---------------------------
+socket.on('user-joined', (newUserId, newUserName) => {
+    console.log(`A new user connected: ${newUserName} [${newUserId}].`)
+
+    users[newUserId] = newUserName
+
+    const newUserCall = peer.call(newUserId, currentUserVideoStream);
+    const newUserVideo = document.createElement('video');
+
+    newUserCall.on('stream', newUserVideoStream => {
+        addVideoStream(newUserVideo, newUserVideoStream);
+    });
+});
+
+socket.on('user-messaged', (message, userId) => {
+    console.log(`${users[userId]} messaged: ${message}`);
+
+    $('.messages').append(`<li class='message'><b>${users[userId]}</b><br/>${message}</li>`);
+    scrollToBottom();
+});
+
+// ---------------------------
+peer.on('open', userId => {
+    joinConfirmDialog.showModal()
+
+    joinBtn.addEventListener('click', e => {
+        e.preventDefault()
+        joinConfirmDialog.close();
+
+        const nameForJoin = nameInputForJoin.value
+
+        users[userId] = nameForJoin
+        socket.emit('join-room', ROOM_ID, userId, nameForJoin);
+        addVideoStream(currentUserVideo, currentUserVideoStream, nameForJoin)
+    })
+});
+peer.on('call', otherUserCall => {
+    otherUserCall.answer(currentUserVideoStream);
+    const otherUserVideo = document.createElement('video');
+
+    otherUserCall.on('stream', otherUserVideoStream => {
+        addVideoStream(otherUserVideo, otherUserVideoStream);
+    });
+});
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 import { v4 as uuidv4 } from 'uuid'
-import { ExpressPeerServer } from 'peer'
+import { ExpressPeerServer as PeerServer } from 'peer'
 import express from 'express'
+import { Server as HttpServer } from 'http'
+import { Server as IoServer } from 'socket.io'
 
 // configuration
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const peerServer = ExpressPeerServer(server, {
+const server = HttpServer(app);
+const io = new IoServer(server);
+const peerServer = PeerServer(server, {
     debug: true,
 });
 
 // configuring app
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use('/peerjs', peerServer)
 
 // routes configuration
@@ -25,12 +27,18 @@ app.get('/:room', (req, res) => {
 
 // socket io configuration
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
+    console.log('New IO connection.')
+
+    socket.on('join-room', (roomId, userId, userName) => {
+        console.log(`A connection joined a room - ${userName} [${userId}].`)
+
         socket.join(roomId);
-        socket.to(roomId).emit('user-connected', userId);
+        socket.to(roomId).emit('user-joined', userId, userName);
 
         socket.on('message', message => {
-            io.to(roomId).emit('create-message', message);
+            console.log(`${userId} ${message}`)
+
+            io.to(roomId).emit('user-messaged', message, userId);
         });
     });
 });
