1719e7cf1d1b160f5d8f86bd4ca8c061e997d4cf
Manbir <manbirjudge2009@gmail.com>
2026-01-13 22:06:36 +0530

    HUGE update

---
+++ README.md
@@ -0,0 +1,20 @@
+# Zoom Clone
+## TODOs
+- Don't use JQuery.
+- Don't use Bootstrap.
+- ~~Don't use FontAwesome.~~
+- Some more info on home page.
+- Better about page.
+- ~~Optimize room layout.~~
+- Keep track of rooms (in memory).
+    - Room names.
+    - Passwords.
+    - Max rooms and memebers limit.
+    - Chat history.
+- Participants list.
+- Voice and video toggles.
+- Maybe - Video filters
+- Notifications (inside the chat) for all events (not just messages).
+- Icons
+- Optimize layout for mobile (landscape only).
+- Validation and error handling.
--- server.js
+++ server.js
@@ -1,41 +1,50 @@
-import { v4 as uuidv4 } from 'uuid'
-import { ExpressPeerServer as PeerServer } from 'peer'
-import express from 'express'
-import { Server as HttpServer } from 'http'
-import { Server as IoServer } from 'socket.io'
+import { v4 as uuidv4 } from "uuid";
+import { Server as HttpServer } from "http";
+import express from "express";
+import expressLayouts from "express-ejs-layouts";
+import { Server as IoServer } from "socket.io";
+import { ExpressPeerServer } from "peer";
 
-// configuration
 const app = express();
 const server = HttpServer(app);
-const io = new IoServer(server);
-const peerServer = PeerServer(server, {
-    debug: true,
+const ioServer = new IoServer(server);
+const peerServer = ExpressPeerServer(server, { debug: true });
+
+const port = process.env.PORT || 8080;
+
+// express
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(expressLayouts);
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Zoom Clone", nextRoomId: uuidv4() });
 });
 
-// configuring app
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use('/peerjs', peerServer)
-
-// routes configuration
-app.get('/', (req, res) => {
-    res.render('index', { nextRoomId: uuidv4() });
-});
-app.get('/rooms/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room });
+app.get("/about", (req, res) => {
+    res.render("about", { title: "About - Zoom Clone", nextRoomId: uuidv4() });
 });
 
-// socket io configuration
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId, userName) => {
+app.get("/room/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room, layout: false });
+});
+
+// socket.io
+ioServer.on("connection", socket => {
+    socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
-        socket.to(roomId).emit('user-joined', userId, userName);
+        socket.to(roomId).emit("user-joined", userId, userName);
 
-        socket.on('message', message => {
-            io.to(roomId).emit('user-messaged', message, userId);
+        socket.on("message", msg => {
+            ioServer.to(roomId).emit("user-messaged", msg, userId);
         });
     });
 });
 
-// starting the server
-server.listen(process.env.PORT || 3030);
+// peerjs
+app.use("/peerjs", peerServer);
+
+// starting
+server.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+});
